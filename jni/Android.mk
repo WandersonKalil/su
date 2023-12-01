9b4ff6d959b12a6babfb24bc60eabec32bd65341
WandersonKalil <117590532+WandersonKalil@users.noreply.github.com>
2023-12-01 11:05:00 -0300

    Add Android.mk

---
+++ jni/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := su
+LOCAL_SRC_FILES := su.c db.c activity.c utils.c daemon.c pts.c
+
+LOCAL_STATIC_LIBRARIES := \
+    liblog \
+    libc \
+
+LOCAL_LDLIBS := -llog -lc
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng debug
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+include $(BUILD_EXECUTABLE)
